df5bf3c9b25365efd119260df8b8ff84e782fe49
Nexor Hub <nexorhub3@gmail.com>
2026-02-04 11:47:19 -0300

    Create Escape Tsunami for Brainrots SCRIPT | Speed X Hub.lua

---
+++ Escape Tsunami for Brainrots SCRIPT | Speed X Hub.lua
@@ -0,0 +1,2 @@
+-- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
