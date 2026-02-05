78f2a0c57617a9f1b4f0a577a26075fdefb5fe85
Nexor Hub <nexorhub3@gmail.com>
2026-02-05 15:09:25 -0300

    Create The Forge [BETA] SCRIPT | Ather Hub.lua

---
+++ The Forge [BETA] SCRIPT | Ather Hub.lua
@@ -0,0 +1,2 @@
+-- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2529a5f9dfddd5523ca4e22f21cceffa.lua"))()
