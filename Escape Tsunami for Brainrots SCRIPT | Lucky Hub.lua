29110da5e1d713b6c249dbe664fa6e888b31999d
Nexor Hub <nexorhub3@gmail.com>
2026-02-05 14:34:44 -0300

    Create Escape Tsunami for Brainrots SCRIPT | Lucky Hub.lua

---
+++ Escape Tsunami for Brainrots SCRIPT | Lucky Hub.lua
@@ -0,0 +1,3 @@
+-- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1heo2fine/idk/main/script.lua"))()
+    
