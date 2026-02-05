1f0d7f964e05faa25897d37caf866ea911df97db
Nexor Hub <nexorhub3@gmail.com>
2026-02-05 14:31:11 -0300

    Create Escape Tsunami for Brainrots SCRIPT | Ekuve Hub.lua

---
+++ Escape Tsunami for Brainrots SCRIPT | Ekuve Hub.lua
@@ -0,0 +1,3 @@
+-- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ekuve/ekuvehub/main/main.lua"))()
+    
