b03a5e8496d92d28cf6f0e14cc550979e22cc132
Nexor Hub <nexorhub3@gmail.com>
2026-02-05 15:12:38 -0300

    Create The Forge [BETA] SCRIPT | Polsec Hub.lua

---
+++ The Forge [BETA] SCRIPT | Polsec Hub.lua
@@ -0,0 +1,2 @@
+-- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Rx1m/CpsHub/refs/heads/main/TheForge"))()
