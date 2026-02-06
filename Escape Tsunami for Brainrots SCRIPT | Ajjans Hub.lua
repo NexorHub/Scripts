3b418e09c082f6d747bfefe9820197d52e02a221
Nexor Hub <nexorhub3@gmail.com>
2026-02-06 15:35:43 -0300

    Create Escape Tsunami for Brainrots SCRIPT | Ajjans Hub.lua

---
+++ Escape Tsunami for Brainrots SCRIPT | Ajjans Hub.lua
@@ -0,0 +1,2 @@
+-- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/832af29a13a41951e31129a974a1e8cc.lua"))()
