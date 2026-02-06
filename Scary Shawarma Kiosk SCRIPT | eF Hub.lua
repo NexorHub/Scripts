bc15e0ffeb13ca3cfa8c69415699cd5bf5310947
Nexor Hub <nexorhub3@gmail.com>
2026-02-06 11:48:28 -0300

    Create Scary Shawarma Kiosk SCRIPT | eF Hub.lua

---
+++ Scary Shawarma Kiosk SCRIPT | eF Hub.lua
@@ -0,0 +1,2 @@
+-- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
+loadstring(game:HttpGet('https://api.rubis.app/v2/scrap/wBNEkpigM0J1y5lV/raw', true))()
